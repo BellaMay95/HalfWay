eaa4afb1b69a72eab923f2e735a3419d2747f2d0
Gabrielle Cordray <gabriellelc95@gmail.com>
2018-03-31 22:05:43 -0500

    View Profile mostly Edit Profile

---
+++ src/components/ChangePassword.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { app } from '../base';
+
+export default class ChangePassword extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return ("Change Password Coming Soon!");
+    }
+}
