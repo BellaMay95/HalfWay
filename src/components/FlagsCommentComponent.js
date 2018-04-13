396ac39f95178959327d223598c0a9326177238d
DanielJimenez97 <sassenger21>
2018-04-13 12:51:30 -0500

    view flagged posts and view flagged comments works. Flag can either be ignored or the corresponding post or comment can be removed. next step is working on updating the page when a post is deleted

---
+++ src/components/FlagsCommentComponent.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {Glyphicon, Panel, PanelGroup, Button, Alert } from 'react-bootstrap';
+import { app } from '../base';
+import './ForumComponent.css';
+
+class ForumComponent extends Component{
+  constructor(props){
+      super(props);
+      this.removeComment = this.removeComment.bind(this);
+      this.keepComment = this.keepComment.bind(this);
+      this.databaseOfFlagsC = app.database().ref().child('flaggedComments');
+      this.databaseOfPosts = app.database().ref().child('forum');
+
+      this.state = {
+        flagged_id: props.flagged_id,
+        thread_id: props.thread_id,
+        thread_userName: props.thread_userName,
+        reason_message: props.reason_message,
+        thread_message: props.thread_message,
+        comment_id: props.comment_id,
+        comment_message: props.comment_message,
+
+      }
+  }
+
+  removeComment(thread_id, flagged_id, comment_id){
+
+    this.databaseOfFlagsC.child(this.props.flagged_id).remove();
+    this.databaseOfPosts.child(this.props.thread_id).child('comments').child(this.props.comment_id).remove();
+
+    alert("Post removed.");
+
+  }
+
+  keepComment(flagged_id){
+    this.databaseOfFlagsC.child(this.props.flagged_id).remove();
+    alert("Post kept.");
+  }
+
+  render(){
+    return(
+      <div id = "flag">
+      <PanelGroup key={this.state.thread_id} id={this.state.thread_id}>
+          <Panel>
+              <Panel.Heading>
+                  <Panel.Title componentClass='h3'>Flagged user: {this.state.thread_userName} <br/>Reason: {this.state.reason_message} <br/>Original Post: {this.state.thread_message} </Panel.Title>
+              </Panel.Heading>
+              <Panel.Body>{this.state.comment_message}</Panel.Body>
+              <Panel.Footer>
+                <div>
+                <Button className="deleteComment" bsStyle="danger" onClick={this.removeComment}>
+                  <Glyphicon glyph="minus-sign" style={{padding: '5px'}}/>
+                  Remove Comment
+                </Button>
+                <Button className="keepComment" bsStyle="success" onClick={this.keepComment}>
+                  <Glyphicon glyph="plus-sign" style={{padding: '5px'}}/>
+                  Accept Comment
+                </Button>
+                </div>
+                <div class="clearfix"></div>
+              </Panel.Footer>
+          </Panel>
+      </PanelGroup>
+      </div>
+    )
+  }
+}
+
+export default ForumComponent;
--- src/components/ViewFlags.js
+++ src/components/ViewFlags.js
@@ -1,8 +1,125 @@
 import React, { Component } from 'react';
-//import { render } from 'react-dom';
+import {Button, Panel} from 'react-bootstrap';
+import { app } from '../base';
+import FlagsComponent from './FlagsComponent';
+import FlagsCommentComponent from './FlagsCommentComponent'
 
+var length = 0;
 export default class ViewFlags extends Component {
-    render() {
-        return (<p>Hello! I'm the ViewFlags Component!</p>);
+  constructor(props) {
+    super(props);
+    this.database = app.database().ref().child('flaggedForum');
+    this.databaseC = app.database().ref().child('flaggedComments');
+
+    this.state = {
+      flagsArr: [],
+      flaggedCommentsArr: [],
+    }
+  }
+
+  componentWillMount(length){
+
+    //section  begins for setting up flagged posts
+    var flaggedPosts = this.state.flagsArr;
+      // Set previousForum to current state
+
+    // Get DataSnapshot every time a child is added to the array
+    this.database.on('child_added', snap => {
+      flaggedPosts.push({
+        id: snap.key,
+        thread_id: snap.val().thread_id,
+        //author_name: snap.val().author_name,
+        reason_message: snap.val().reason_message,
+        thread_message: snap.val().thread_message,
+        thread_userName: snap.val().thread_userName,
+      })
+
+      //reverse the array to show newest posts first
+      //prevJob = prevJob.reverse();
+      // Push the array that we have just updated (previousForum) to the state
+      this.setState({
+        flagsArr: flaggedPosts,
+      })
+    })
+
+//section begings for setting up getting comment flags
+      var flaggedComments = this.state.flaggedCommentsArr;
+        // Set previousForum to current state
+
+      // Get DataSnapshot every time a child is added to the array
+      this.databaseC.on('child_added', snap => {
+        flaggedComments.push({
+          id: snap.key,
+          thread_id: snap.val().thread_id,
+          reason_message: snap.val().reason_message,
+          thread_message: snap.val().thread_message,
+          thread_userName: snap.val().thread_userName,
+          comment_message: snap.val().comment_message,
+          comment_id: snap.val().comment_id,
+        })
+        length = length+1;
+        //reverse the array to show newest posts first
+        //prevJob = prevJob.reverse();
+        // Push the array that we have just updated (previousForum) to the state
+        this.setState({
+          flaggedCommentsArr: flaggedComments,
+        })
+      })
+
+
+  }
+
+
+
+    render(){
+      if (this.state.flagsArr.length === 0 && this.state.flaggedCommentsArr.length === 0)
+      {
+        return(<h1>Currently NO Flagged Posts or Comments</h1>);
+
+      }
+      return(
+        <div>
+        <Panel>
+
+        {/*.map function runs through all flagged posts and sends them to the flag component*/}
+        <Panel.Body>
+
+        {
+
+
+
+        this.state.flagsArr.map((post , index) => {
+            if (index === 0){
+              return(<div><h1>Flagged Posts</h1><FlagsComponent flagged_id = {post.id}  thread_id={post.thread_id} reason_message={post.reason_message} thread_message={post.thread_message} thread_userName={post.thread_userName}/></div>);
+            }
+            return(
+              <FlagsComponent flagged_id = {post.id}  thread_id={post.thread_id} reason_message={post.reason_message} thread_message={post.thread_message} thread_userName={post.thread_userName}/>
+            )
+        })
+
+
+        }
+        </Panel.Body>
+        </Panel>
+        {/*.map function runs through all flagged comments and sends them to the flag component*/}
+
+        <Panel>
+        <Panel.Body>
+        {
+        this.state.flaggedCommentsArr.map((com , index) => {
+            if (index === 0){
+              return(<div><h1>Flagged Comments</h1><FlagsCommentComponent flagged_id = {com.id}  thread_id={com.thread_id} reason_message={com.reason_message} thread_message={com.thread_message} thread_userName={com.thread_userName} comment_id={com.comment_id} comment_message={com.comment_message}/></div>);
+            }
+            return(
+              <FlagsCommentComponent flagged_id = {com.id}  thread_id={com.thread_id} reason_message={com.reason_message} thread_message={com.thread_message} thread_userName={com.thread_userName} comment_id={com.comment_id} comment_message={com.comment_message}/>
+            )
+        })
+        }
+        </Panel.Body>
+        </Panel>
+        </div>
+
+      );
+
     }
 }
