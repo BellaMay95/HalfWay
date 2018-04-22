8901cd3157bfb7c4f7ade9868ecff6074720e34a
Gabrielle Cordray <gabriellelc95@gmail.com>
2018-04-22 01:54:16 -0500

    tests for resources admin profile view/edit

--- tests/auth-flow.js
+++ tests/auth-flow.js
@@ -7,9 +7,9 @@ module.exports = {
 
 	'get to login page': (browser) => {
 		// Load the page at the launch URL
-		browser
-			.url(browser.launchUrl)
-			.assert.urlEquals('http://localhost:3000/login'); //change for prod server
+		browser.url(browser.launchUrl);
+		browser.assert.urlEquals('http://localhost:3000/login'); //change for prod server
+		browser.waitForElementPresent('input[name="email"]', 5000);
 	},
 
 	/*'logging in with empty password': (browser) => {
@@ -83,15 +83,53 @@ module.exports = {
 		browser.clearValue('input[name="password"]');
 	},*/
 
-	'logging in with valid credentials': (browser) => {
+	'logging in with valid credentials as admin': (browser) => {
 		browser.setValue('input[name="email"]', "halfway@halfway.com");
 		browser.setValue('input[name="password"]', "Password1!");
 		browser.click('#loginButton');
 		browser.pause(3000);
 		browser.assert.urlEquals('http://localhost:3000/'); //change for prod server
 	},
+
+	'checking for admin panel (as admin)': (browser) => {
+		browser.expect.element('#admin').to.be.present;
+	},
+
+	'checking for resources add/edit (as admin)': (browser) => {
+		browser.click('#resources');
+		browser.click('#resources-list-heading-2');
+		browser.expect.element('.btn-info').to.be.present;
+		browser.expect.element('.deleteResource').to.be.present;
+	},
+
 	
-	'logging out': (browser) => {
+	'logging out (admin)': (browser) => {
+		browser.click('a[href="/logout"]');
+		browser.assert.urlEquals('http://localhost:3000/logout');
+		browser.pause(1000);
+		browser.assert.urlEquals('http://localhost:3000/login');
+	},
+
+	'logging in with valid credentials as youth': (browser) => {
+		browser.setValue('input[name="email"]', "jessie4ever@halfway.com");
+		browser.setValue('input[name="password"]', "Password1!");
+		browser.click('#loginButton');
+		browser.pause(3000);
+		browser.assert.urlEquals('http://localhost:3000/'); //change for prod server
+	},
+
+	'checking for admin panel (as youth)': (browser) => {
+		browser.expect.element('#admin').to.not.be.present;
+	},
+
+	'checking for resources add/edit (as youth)': (browser) => {
+		browser.click('#resources');
+		browser.click('#resources-list-heading-2');
+		browser.expect.element('.btn-info').to.not.be.present;
+		browser.expect.element('.deleteResource').to.not.be.present;
+	},
+
+	'logging out (youth)': (browser) => {
 		browser.click('a[href="/logout"]');
 		browser.assert.urlEquals('http://localhost:3000/logout');
 		browser.pause(1000);
--- tests/forum-flow.js
+++ tests/forum-flow.js
@@ -1,4 +1,14 @@
 module.exports = {
+    before: (browser) => {
+        browser.url(browser.launchUrl).assert.urlEquals('http://localhost:3000/login'); //change for prod server
+        browser.waitForElementPresent('input[name="email"]', 5000);
+        browser.setValue('input[name="email"]', "halfway@halfway.com");
+        browser.setValue('input[name="password"]', "Password1!");
+        browser.click('#loginButton');
+        browser.pause(3000);
+        browser.assert.urlEquals('http://localhost:3000/'); //change for prod server
+    },
+    
     beforeEach : function(browser) {
 		//console.log("refreshing window!");
 		browser.refresh();
